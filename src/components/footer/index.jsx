6c193c13052ae40bc4770a6b3437e8e4f314c3d2
unknown <euclidesjoao.dev@gmail.com>
2023-12-02 13:27:25 +0100

    removing the grey backgound on footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,61 +1,50 @@
 import './index.css'
 import { NavLink } from "react-router-dom"
-import { IconButton } from "@mui/material"
+import { FaFacebook, FaInstagram, FaYoutube, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
-import { LinkedIn, Twitter, Youtube, Instagram, GitHub, Facebook } from '../icons'
 
 export const Footer=()=> {
+
   return (
     <div className='container-fluid footer'>
 
       <div className='socialmedia'>
 
-        <ul className="d-flex">
+        <ul className="d-flex align-elements-center justify-content-center">
 
           <li>
-              <NavLink to='https://www.facebook.com/euclides.manuel.3386/' target='_blank'>
-                <IconButton>
-                  <Facebook className="icon"/>
-                </IconButton>            
-              </NavLink>
-          </li>
-
-          <li>
-              <NavLink to='https://www.instagram.com/euclidesjoao.dev/' target='_blank'>
-                <IconButton>
-                  <Instagram className="icon"/>
-                </IconButton>
-              </NavLink>
-          </li>
-
-          <li>
-              <NavLink>
-                <IconButton>
-                  <LinkedIn className="icon"/>
-                </IconButton>
-              </NavLink>
-          </li>
-          <li>
-            <NavLink to='https://github.com/John-3-3' target='_blank'>
-              <IconButton>
-                <GitHub className="icon"/>
-              </IconButton>
+            <NavLink to='https://www.facebook.com/euclides.manuel.3386/' target='_blank'>
+              <FaFacebook className='icon'/>
             </NavLink>
           </li>
 
           <li>
-              <NavLink to='https://www.youtube.com/channel/UCXsHxZWa8eX3GAk-xqIviZw' target='_blank'>
-                <IconButton>
-                  <Youtube className="icon"/>
-                </IconButton>
+            <NavLink to='https://www.instagram.com/euclidesjoao.dev/' target='_blank'>
+              <FaInstagram className='icon'/>
+            </NavLink>
+          </li>
+
+          <li>
+              <NavLink>
+                <FaLinkedin className='icon'/>
               </NavLink>
           </li>
 
           <li>
+            <NavLink to='https://github.com/John-3-3' target='_blank'>
+              <FaGithub className='icon'/>
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink to='https://www.youtube.com/channel/UCXsHxZWa8eX3GAk-xqIviZw' target='_blank'>
+              <FaYoutube className='icon'/>
+            </NavLink>
+          </li>
+
+          <li>
             <NavLink to='https://twitter.com/Mr_Euclides' target='_target'>
-              <IconButton>
-                <Twitter className="icon"/>
-              </IconButton>
+              <FaTwitter className='icon'/>
             </NavLink>
           </li>
         </ul>
